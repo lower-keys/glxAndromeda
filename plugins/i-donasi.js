6a59675461b8f3619b104f5d87808f5fa01576a4
lower-keys <lowerkeys9@gmail.com>
2022-09-19 21:34:14 +0700

    aaa

--- plugins/i-donasi.js
+++ plugins/i-donasi.js
@@ -8,16 +8,14 @@ let handler = async (m, { conn, usedPrefix }) => {
       extendedTextMessage: {
       text: `
 ╭─「 Donasi • Dana 」
-│ • Triii [62895604670507]
-│ • Gopay  [62895604670507]
-│ • Dana  [62895604670507]
+│ • Axis [6283837261215]
+│ • Gopay  ga sempet buat
+│ • Dana  sama aja kaya gopay 
 │ • Qris  [Chat OWNER]
 ╰────
-╭─「 *NOTE* 」
-│ > Ingin donasi? Wa.me/62895604670507
-│ _Hasil donasi akan digunakan buat sewa_
-│ _atau beli *RDP/VPS* agar bot bisa jalan_
-│ _24jam tanpa kendala_
+
+ *NOTE*
+ Gunakan Bot sebijak Mungkin
 ╰────
 `,
       contextInfo: {
